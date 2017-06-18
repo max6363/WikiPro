7b0d16cc56490da8baa84d2ed047776e185c6388
Minhaz Panara <minhaz.panara@gmail.com>
2017-06-18 17:33:56 +0530

    ADDED - Detail : links added.

--- WikiPro/ViewControllers/Explore/ParagraphCell.h
+++ WikiPro/ViewControllers/Explore/ParagraphCell.h
@@ -8,7 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol ParagraphCellDelegate;
+
 @interface ParagraphCell : UITableViewCell
+
 @property (weak, nonatomic) IBOutlet UILabel *lblTitle;
+@property (weak, nonatomic) IBOutlet FRHyperLabel *lblTitleWithLinks;
+@property (nonatomic) NSMutableDictionary *dataDict;
+@property (nonatomic) id <ParagraphCellDelegate> delegate;
+
+- (void)updateLinksAndText;
+
+@end
+
+@protocol ParagraphCellDelegate <NSObject>
+
+@optional
+- (void)didSelectLinkWithText:(NSString *)text;
 
 @end
--- WikiPro/ViewControllers/Explore/ParagraphCell.m
+++ WikiPro/ViewControllers/Explore/ParagraphCell.m
@@ -21,4 +21,25 @@
     // Configure the view for the selected state
 }
 
+- (void)updateLinksAndText
+{
+    //Step 1: Define a normal attributed string for non-link texts
+    NSString *string = self.dataDict[___kDataKeyText];
+    NSDictionary *attributes = @{NSFontAttributeName: _lblTitleWithLinks.font};
+    
+    _lblTitleWithLinks.attributedText = [[NSAttributedString alloc]initWithString:string attributes:attributes];
+    
+    
+    //Step 2: Define a selection handler block
+    void(^handler)(FRHyperLabel *label, NSString *substring) = ^(FRHyperLabel *label, NSString *substring){
+        if (self.delegate && [self.delegate respondsToSelector:@selector(didSelectLinkWithText:)]) {
+            [self.delegate didSelectLinkWithText:substring];
+        }
+    };
+    
+    NSArray *clickableStrings = self.dataDict[___kDataKeyTextClickableTokens];
+    //Step 3: Add link substrings
+    [_lblTitleWithLinks setLinksForSubstrings:clickableStrings withLinkHandler:handler];
+}
+
 @end
